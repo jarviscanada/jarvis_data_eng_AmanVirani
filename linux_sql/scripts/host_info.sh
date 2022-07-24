d2c96041467f7898b83599a2dcdf72f0592c34cf
Aman Virani <amanvirani98@gmail.com>
2022-07-24 22:14:06 +0000

    changes in the host_info for total memory

--- linux_sql/scripts/host_info.sh
+++ linux_sql/scripts/host_info.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#! /bin/bash
 
 psql_host=$1
 psql_port=$2
@@ -20,7 +20,8 @@ cpu_architecture=$(echo "$lscpu_out" | egrep "^Architecture" | awk '{print $2}' 
 cpu_model=$(echo "$lscpu_out" | egrep "^Model name" | awk '{print $3 " " $4 " " $5}' | xargs)
 cpu_mhz=$(echo "$lscpu_out" | egrep "^CPU MHz" | awk '{print $3}' | xargs)
 l2_cache=$(echo "$lscpu_out" | egrep "^L2 cache" | awk '{print $3}' | sed 's/.$//' | xargs)
-total_mem=$(cat /proc/meminfo | egrep "^MemTotal" | awk '{print $2}' | xargs)
+lscpu_out = `cat /proc/meminfo`
+total_mem=$(echo "$lscpu_out" | egrep "^MemTotal" | awk '{print $2}' | xargs)
 timestamp=$(date "+%D %T")
 
 insert_stmt="INSERT INTO host_info(hostname, cpu_number, cpu_architecture, cpu_model, cpu_mhz, l2_cache, total_mem, timestamp)
