888ed368c77d3733150e2f7d9bce9641e418512c
Aman Virani <amanvirani98@gmail.com>
2022-09-15 02:28:55 +0000

    Created test cases

--- core_java/twitter/src/main/java/ca/jrvs/apps/twitter/dao/TwitterHttpHelper.java
+++ core_java/twitter/src/main/java/ca/jrvs/apps/twitter/dao/TwitterHttpHelper.java
@@ -80,15 +80,5 @@ public class TwitterHttpHelper implements HttpHelper {
     }
   }
 
-  public static void main(String[] args) throws Exception{
-    String CONSUMER_KEY = System.getenv("consumerKey");
-    String CONSUMER_SECRET = System.getenv("consumerSecret");
-    String ACCESS_TOKEN = System.getenv("accessToken");
-    String TOKEN_SECRET = System.getenv("tokenSecret");
-    System.out.println(CONSUMER_KEY +" | "+ CONSUMER_SECRET +" | "+ ACCESS_TOKEN +" | "+ TOKEN_SECRET);
-    HttpHelper httpHelper = new TwitterHttpHelper(CONSUMER_KEY,CONSUMER_SECRET,ACCESS_TOKEN,TOKEN_SECRET);
-    HttpResponse response = httpHelper.httpPost(new URI("https://api.twitter.com/1.1/statuses/update.json?status=testing"));
-    System.out.println(EntityUtils.toString(response.getEntity()));
-  }
 
 }
